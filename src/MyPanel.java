be018e1baf39d2a79b129793e5f573436d9ffe65
student <frolovov>
2023-03-03 12:46:49 +0300

    Вывод цифр

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -13,34 +13,56 @@ public class MyPanel extends JPanel {
 
     private int x = 100;
     private int y = 100;
-    private int width = 200;
-    private int height = 200;
-    private int delta = 10;
+    private int width = 50;
+    private int height = 100;
+    private int step = 30;
+    private int delta = 5;
 
     private ArrayList<Rectangle> outList = new ArrayList<Rectangle>();
 
     public void setSymbols(String parm) {
-        for (int i = 1; i < 8; i++) {
-            segment(i);
+        String[] arr = parm.replace(",", "").split(" ");
+        for (String cifra:arr) {
+            switch(cifra) {
+                case "1":
+                    segment(2);
+                    segment(4);
+                    break;
+                case "2":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(5);
+                    segment(6);
+                    break;
+                case "3":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(4);
+                    segment(5);
+                    break;
+            }
+            x = x + step + width;
         }
     }
 
     private void segment(int number) {
         switch(number) {
             case 1:
-                outList.add(new Rectangle(x + delta,y,width-delta,y));
+                outList.add(new Rectangle(x + delta,y,x+width-delta,y));
                 break;
             case 2:
-                outList.add(new Rectangle(width,y+delta,width,y+(height/2)-delta));
+                outList.add(new Rectangle(x+width,y+delta,x+width,y+(height/2)-delta));
                 break;
             case 3:
-                outList.add(new Rectangle(x + delta,y+height/2,width-delta,y+height/2));
+                outList.add(new Rectangle(x + delta,y+height/2,x+width-delta,y+height/2));
                 break;
             case 4:
-                outList.add(new Rectangle(width,y+(height/2)+delta,width,y+height-delta));
+                outList.add(new Rectangle(x+width,y+(height/2)+delta,x+width,y+height-delta));
                 break;
             case 5:
-                outList.add(new Rectangle(x + delta,y+height,width-delta,y+height));
+                outList.add(new Rectangle(x + delta,y+height,x+width-delta,y+height));
                 break;
             case 6:
                 outList.add(new Rectangle(x,y+(height/2)+delta,x,y+height-delta));

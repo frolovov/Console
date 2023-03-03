02ecc73bba555a577ad8acd301ae85094cc698f1
student <frolovov>
2023-03-03 12:22:29 +0300

    Вывод семи сегментов

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
 
         JFrame f = new JFrame("");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MyPanel myPanel = new MyPanel();
         myPanel.repaint();
+        myPanel.setSymbols("1, 2, 3");
         f.add(myPanel);
         f.pack();
         f.setVisible(true);
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class MyPanel extends JPanel {
     public MyPanel() {
@@ -7,17 +8,54 @@ public class MyPanel extends JPanel {
     }
 
     public Dimension getPrefferedSize() {
-        return new Dimension(1500, 700);
+        return new Dimension(1560,700);
     }
 
-    private int x = 0;
-    private int y = 0;
-    private int width = 100;
-    private int height = 100;
+    private int x = 100;
+    private int y = 100;
+    private int width = 200;
+    private int height = 200;
+    private int delta = 10;
+
+    private ArrayList<Rectangle> outList = new ArrayList<Rectangle>();
+
+    public void setSymbols(String parm) {
+        for (int i = 1; i < 8; i++) {
+            segment(i);
+        }
+    }
+
+    private void segment(int number) {
+        switch(number) {
+            case 1:
+                outList.add(new Rectangle(x + delta,y,width-delta,y));
+                break;
+            case 2:
+                outList.add(new Rectangle(width,y+delta,width,y+(height/2)-delta));
+                break;
+            case 3:
+                outList.add(new Rectangle(x + delta,y+height/2,width-delta,y+height/2));
+                break;
+            case 4:
+                outList.add(new Rectangle(width,y+(height/2)+delta,width,y+height-delta));
+                break;
+            case 5:
+                outList.add(new Rectangle(x + delta,y+height,width-delta,y+height));
+                break;
+            case 6:
+                outList.add(new Rectangle(x,y+(height/2)+delta,x,y+height-delta));
+                break;
+            case 7:
+                outList.add(new Rectangle(x,y+delta,x,y+(height/2)-delta));
+                break;
+        }
+    }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawLine(x, y, width, height);
+        for(Rectangle rect: outList) {
+            g.drawLine(rect.x, rect.y, rect.width, rect.height);
+        }
     }
 }

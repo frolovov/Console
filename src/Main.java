c59b88efda26cca4dd6445011f060e5292f5c6a0
dopnet9203 <dopnet9203@gmail.com>
2023-03-10 00:45:33 +0300

    Ввод данных во время работы программы

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,8 @@ public class Main {
         int[] koords = new int[5];
         JTextField smallField = new JTextField("300, 250, 25, 45, 20");
         JTextField numbers = new JTextField("0, 1, 2, 3, 4, 5, 6, 7, 8, 9,");
-
+        JFrame f = new JFrame("Swing Paint Demo");
+        MyPanel myPanel = new MyPanel();
         numbers.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -21,10 +22,22 @@ public class Main {
                     JOptionPane.showMessageDialog(null,
                             "Ошибка в количестве параметров");
                 }
+                for (int i = 0; i < parm.length; i++) {
+                    koords[i] = getInteger(parm[i]);
+                }
+                myPanel.setX(koords[0]);
+                myPanel.setY(koords[1]);
+                myPanel.setWidth(koords[2]);
+                myPanel.setHeight(koords[3]);
+                myPanel.setStep(koords[4]);
+                myPanel.setSymbols(numbers.getText());
+                myPanel.repaint();
+                f.add(myPanel);
+                f.pack();
+                f.setVisible(true);
             }
         });
 
-        JFrame f = new JFrame("Swing Paint Demo");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel contents = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
@@ -32,11 +45,19 @@ public class Main {
         contents.add(smallField);
         f.setContentPane(contents);
 
-        MyPanel myPanel = new MyPanel();
         myPanel.repaint();
-        myPanel.setSymbols("1, 2, 3");
         f.add(myPanel);
         f.pack();
         f.setVisible(true);
+    }
+
+    private static int getInteger(String parm) {
+        try {
+            return Integer.parseInt(parm);
+        }
+        catch (Exception e) {
+            new Exception("Это не цифра");
+        }
+        return 0;
     }
 }

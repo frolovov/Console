4b2752c75b8702b5b7576449bae1ae042d835e38
dopnet9203 <dopnet9203@gmail.com>
2023-03-10 00:34:51 +0300

    Панели с текстовыми полями

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        JFrame f = new JFrame("");
+        int[] koords = new int[5];
+        JTextField smallField = new JTextField("300, 250, 25, 45, 20");
+        JTextField numbers = new JTextField("0, 1, 2, 3, 4, 5, 6, 7, 8, 9,");
+
+        numbers.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String[] parm = smallField.getText().replaceAll(" ",
+                        "").split(",");
+                int[] koords = new int[parm.length];
+                if (parm.length != parm.length) {
+                    JOptionPane.showMessageDialog(null,
+                            "Ошибка в количестве параметров");
+                }
+            }
+        });
+
+        JFrame f = new JFrame("Swing Paint Demo");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel contents = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        contents.add(numbers);
+        contents.add(smallField);
+        f.setContentPane(contents);
+
         MyPanel myPanel = new MyPanel();
         myPanel.repaint();
         myPanel.setSymbols("1, 2, 3");
